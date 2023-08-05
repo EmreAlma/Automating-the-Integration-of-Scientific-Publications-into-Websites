2c1ff460cbbefa1eb2cb83cced486b344f164532
EmreAlma <emrealma@hotmail.com>
2023-08-05 03:54:23 +0200

    Modify PublicationController for new PublicationView conversion

--- pubsync/src/main/java/com/example/pubsync/controller/PublicationController.java
+++ pubsync/src/main/java/com/example/pubsync/controller/PublicationController.java
@@ -1,7 +1,8 @@
 package com.example.pubsync.controller;
 
-import com.example.pubsync.entity.Publication;
+import com.example.pubsync.model.PublicationView;
 import com.example.pubsync.repository.PublicationRepository;
+import com.example.pubsync.service.ConverterService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +13,16 @@ import java.util.List;
 public class PublicationController {
 
     private final PublicationRepository publicationRepository;
+    private final ConverterService converterService;
 
-    public PublicationController(PublicationRepository publicationRepository) {
+    public PublicationController(PublicationRepository publicationRepository, ConverterService converterService) {
         this.publicationRepository = publicationRepository;
+        this.converterService = converterService;
     }
 
     @GetMapping("/getbyyear")
-    public List<Publication> getPublicationByYear(@RequestParam(value = "year") String year){
+    public List<PublicationView> getPublicationByYear(@RequestParam(value = "year") String year){
 
-        return publicationRepository.findPublicationsByYear(year);
+        return converterService.convertPublicationViewList(publicationRepository.findPublicationsByYear(year));
     }
 }
