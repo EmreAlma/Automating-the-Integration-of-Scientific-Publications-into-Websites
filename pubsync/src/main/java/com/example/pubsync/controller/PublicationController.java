a8ce38fc1ec2f8a12d9fca27c6cf68d07a2cd807
EmreAlma <emrealma@hotmail.com>
2023-08-05 04:56:19 +0200

    Add Endpoint for Retrieving Publications Sorted by Year

--- pubsync/src/main/java/com/example/pubsync/controller/PublicationController.java
+++ pubsync/src/main/java/com/example/pubsync/controller/PublicationController.java
@@ -4,12 +4,15 @@ import com.example.pubsync.model.PublicationView;
 import com.example.pubsync.repository.PublicationRepository;
 import com.example.pubsync.service.ConverterService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/publications")
 public class PublicationController {
 
     private final PublicationRepository publicationRepository;
@@ -24,5 +27,18 @@ public class PublicationController {
     public List<PublicationView> getPublicationByYear(@RequestParam(value = "year") String year){
 
         return converterService.convertPublicationViewList(publicationRepository.findPublicationsByYear(year));
+    }
+
+    @GetMapping("/all")
+    public List<PublicationView> getAllPublications() {
+        List<PublicationView> allPublications = new ArrayList<>();
+
+        for (int year = 2020; year <= 2023; year++) {
+            String yearStr = String.valueOf(year);
+            List<PublicationView> publications = converterService.convertPublicationViewList(publicationRepository.findPublicationsByYear(yearStr));
+            allPublications.addAll(publications);
+        }
+
+        return allPublications;
     }
 }
