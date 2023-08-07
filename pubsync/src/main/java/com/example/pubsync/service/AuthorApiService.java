6d5020c8e12ee48edd368c45c2f46fa2932f3ddb
EmreAlma <emrealma@hotmail.com>
2023-08-07 23:07:09 +0200

    Provide documentation for AuthorApiService and AuthorPublicationService classes and methods.

--- pubsync/src/main/java/com/example/pubsync/service/AuthorApiService.java
+++ pubsync/src/main/java/com/example/pubsync/service/AuthorApiService.java
@@ -10,15 +10,29 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * This class provides a service for interacting with the DBLP API (https://dblp.org),
+ * facilitating the retrieval of author information from a data source and parsing the response using Gson.
+ */
 @Service
 public class AuthorApiService {
-
+    /**
+     * Gson object for JSON parsing.
+     */
     private final Gson gson;
-
+    /**
+     * Constructs an AuthorApiService with the provided Gson object.
+     * @param gson Gson object for JSON parsing.
+     */
     public AuthorApiService(Gson gson) {
         this.gson = gson;
     }
 
+    /**
+     * Retrieves author information from the external API based on the parameter.
+     * @param param Search parameter for author page.
+     * @return Parsed Response object from the API.
+     */
     public Response getAuthorsPage(String param) {
         HttpClient httpClient = HttpClient.newBuilder()
                 .build();
--- pubsync/src/main/java/com/example/pubsync/service/AuthorPublicationService.java
+++ pubsync/src/main/java/com/example/pubsync/service/AuthorPublicationService.java
@@ -1,6 +1,5 @@
 package com.example.pubsync.service;
 
-
 import com.example.pubsync.entity.Author;
 import com.example.pubsync.model.Response;
 import com.example.pubsync.repository.AuthorRepository;
@@ -8,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/** This class responsible for fetching and saving publications for authors and
+ *  interacting with the publication database.
+ */
 @Service
 public class AuthorPublicationService {
 
@@ -16,13 +18,20 @@ public class AuthorPublicationService {
     private final PublicationDatabaseManager publicationDatabaseManager;
 
     private  final AuthorRepository authorRepository;
+
+    /** Constructor for AuthorPublicationService class.
+     * @param authorApiService  Service for interacting with the author API.
+     * @param publicationDatabaseManager    Manager for interacting with the publication database.
+     * @param authorRepository  Repository for accessing author data.
+     */
     public AuthorPublicationService(AuthorApiService authorApiService, PublicationDatabaseManager publicationDatabaseManager, AuthorRepository authorRepository) {
         this.authorApiService = authorApiService;
         this.publicationDatabaseManager = publicationDatabaseManager;
         this.authorRepository = authorRepository;
     }
 
-
+    /** This method retrieves author information, queries DBLP API, and then saves the fetched publications
+     * into the database.*/
     public void fetchAndSavePublicationsForAuthors() {
         List<Author> authors = authorRepository.findAll();
 
@@ -33,6 +42,10 @@ public class AuthorPublicationService {
         }
     }
 
+    /** This method formats the author's name by replacing spaces with underscores to create a valid parameter for API requests.
+     * @param author The author whose name is to be formatted.
+     * @return The formatted name suitable for API requests.
+     */
     private String formatAuthorName(Author author) {
         String firstName = author.getName();
         String lastName = author.getLastName();
