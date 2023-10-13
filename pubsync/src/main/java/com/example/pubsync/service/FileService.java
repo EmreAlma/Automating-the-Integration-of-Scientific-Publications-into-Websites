41f6fff1008926558f692a38d5742f69a14f77ba
EmreAlma <emrealma@hotmail.com>
2023-10-14 01:00:33 +0200

    Create a predefined header section for the markdown file. Adapt markdown formatting to Hugo layout. Implement sorting of publications by year.

--- pubsync/src/main/java/com/example/pubsync/service/FileService.java
+++ pubsync/src/main/java/com/example/pubsync/service/FileService.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
 
 @Service
 public class FileService {
@@ -20,15 +21,42 @@ public class FileService {
     }
 
     public void createMarkdownFile() throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter("/Users/emrealma/Documents/GitHub/Automating-the-Integration-of-Scientific-Publications-into-Websites/pubsync/src/main/resources/templates/publications.md");
-        List<PublicationView> publicationViewList = converterService.convertPublicationViewList(publicationRepository.findAll());
+        try (PrintWriter writer = new PrintWriter("/Users/emrealma/Desktop/testwebrepo-main/content/research/publications/_index.md")) {
+            List<PublicationView> publicationViewList = converterService.convertPublicationViewList(publicationRepository.findAll());
 
-        for (PublicationView publication : publicationViewList) {
-            writer.print(publication.getTitle()+"\n");
-            writer.print(publication.getPublishAuthors()+"\n");
-            writer.print("*"+publication.getVenue()+"\n");
-            writer.print(publication.getYear()+"*"+"\n\n");
-            writer.print("[Link]("+publication.getPublishLink()+")"+"\n\n");
+            writer.println("---");
+            writer.println("title: \"Publications\"");
+            writer.println("bg_image: \"images/2020-landscape-2.png\"");
+            writer.println("description: \"This is meta description\"");
+            writer.println("draft: false");
+            writer.println("weight: 2");
+            writer.println("---");
+            writer.println();
+            writer.println("{{< rawhtml >}}");
+            writer.println("  <p class=\"speshal-fancy-custom\">");
+            writer.println("  </p>");
+            writer.println("{{< /rawhtml >}}");
+            writer.println();
+
+            Map<Integer, List<PublicationView>> publicationsByYear = new TreeMap<>(Comparator.reverseOrder());
+            for (PublicationView publication : publicationViewList) {
+                publicationsByYear.computeIfAbsent(Integer.valueOf(publication.getYear()), k -> new ArrayList<>()).add(publication);
+            }
+
+            for (Map.Entry<Integer, List<PublicationView>> entry : publicationsByYear.entrySet()) {
+                writer.println("## " + entry.getKey());
+                for (PublicationView publication : entry.getValue()) {
+                    writer.println();
+                    writer.println("- " + publication.getPublishAuthors() + ". ");
+                    writer.println(publication.getTitle() + ". _" + publication.getVenue() + ", " + publication.getYear() + "_.  ");
+                    if(publication.getPublishLink().startsWith("http")) {
+                        writer.println("[[Link]](" + publication.getPublishLink() + ") ");
+                    } else {
+                        writer.println("[[Link]](/link/" + publication.getPublishLink() + ") ");
+                    }
+                    writer.println();
+                }
+            }
         }
     }
 }
